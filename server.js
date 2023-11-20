c658aae36a00401dcebe6c0bc1a60da84698552a
Likhith Kanigolla <likhithkanigolla@gmail.com>
2023-11-20 13:09:29 +0530

    Added For Node 2

--- server.js
+++ server.js
@@ -19,11 +19,55 @@ const pool = new Pool({
     port: 5432,
 });
 
-// Endpoint to handle incoming data
+// Endpoint to handle incoming data for node 1
 app.post('/incoming-data', async (req, res) => {
     try {
-        // ... (your existing code)
+        // Extract data from the received JSON
+        const cin = req.body['m2m:sgn']['m2m:nev']['m2m:rep']['m2m:cin'];
+        console.log('Received data:', JSON.stringify(cin, null, 2));
 
+        // Log the "con" value
+        const con = cin['con'];
+        console.log('Original con value:', con);
+
+        // Parse the "con" values
+	const conValues = con
+  	.slice(1, -1) // Remove square brackets
+  	.split(',')
+  	.map(value => parseInt(value.trim(), 10));
+
+        console.log('Parsed con values:', conValues);
+
+        // Check if any value is NaN, and handle it accordingly
+        if (conValues.some(isNaN)) {
+            throw new Error('Invalid con values. Please check the format.');
+        }
+
+        const timestamp = cin['ct'].replace(/(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})/, '$1-$2-$3T$4:$5:$6');
+        const date = new Date(timestamp);
+        date.setMinutes(date.getMinutes() + 660); // Add 5 hours and 30 minutes to the timestamp
+        const formattedTimestamp = date.toISOString().replace('T', ' ').replace(/\.\d+Z$/, '');
+
+        console.log('Formatted timestamp:', formattedTimestamp);
+
+        // Insert data into PostgreSQL database
+        const result = await pool.query(
+            'INSERT INTO sensor_data (timestamp, current_led_red, current_led_green, current_led_blue, saved_sensor_red, saved_sensor_green, saved_sensor_blue, saved_clear, saved_color_temp, saved_lux) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
+            [
+                formattedTimestamp,
+                conValues[0], // current_led_red
+                conValues[1], // current_led_green
+                conValues[2], // current_led_blue
+                conValues[3], // saved_sensor_red
+                conValues[4], // saved_sensor_green
+                conValues[5], // saved_sensor_blue
+                conValues[6], // saved_clear
+                conValues[7], // saved_color_temp
+                conValues[8]  // saved_lux
+            ]
+        );
+
+        console.log('Data inserted successfully:', result.rows[0]);
         res.status(200).send('Data received and inserted successfully.');
     } catch (error) {
         console.error('Error inserting data into PostgreSQL:', error.message);
@@ -31,11 +75,79 @@ app.post('/incoming-data', async (req, res) => {
     }
 });
 
+// Subscription for node 2
+app.post('/incoming-data-2', async (req, res) => {
+    try {
+        // Extract data from the received JSON
+        const cin = req.body['m2m:sgn']['m2m:nev']['m2m:rep']['m2m:cin'];
+        console.log('Received data:', JSON.stringify(cin, null, 2));
+
+        // Log the "con" value
+        const con = cin['con'];
+        console.log('Original con value:', con);
+
+        // Parse the "con" values
+	const conValues = con
+  	.slice(1, -1) // Remove square brackets
+  	.split(',')
+  	.map(value => parseInt(value.trim(), 10));
+
+        console.log('Parsed con values:', conValues);
+
+        // Check if any value is NaN, and handle it accordingly
+        if (conValues.some(isNaN)) {
+            throw new Error('Invalid con values. Please check the format.');
+        }
+
+        const timestamp = cin['ct'].replace(/(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})/, '$1-$2-$3T$4:$5:$6');
+        const date = new Date(timestamp);
+        date.setMinutes(date.getMinutes() + 660); // Add 5 hours and 30 minutes to the timestamp
+        const formattedTimestamp = date.toISOString().replace('T', ' ').replace(/\.\d+Z$/, '');
+
+        console.log('Formatted timestamp:', formattedTimestamp);
+
+        // Insert data into PostgreSQL database
+        const result = await pool.query(
+            'INSERT INTO sensor_data_2 (timestamp, current_led_red, current_led_green, current_led_blue, saved_sensor_red, saved_sensor_green, saved_sensor_blue, saved_clear, saved_color_temp, saved_lux) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
+            [
+                formattedTimestamp,
+                conValues[0], // current_led_red
+                conValues[1], // current_led_green
+                conValues[2], // current_led_blue
+                conValues[3], // saved_sensor_red
+                conValues[4], // saved_sensor_green
+                conValues[5], // saved_sensor_blue
+                conValues[6], // saved_clear
+                conValues[7], // saved_color_temp
+                conValues[8]  // saved_lux
+            ]
+        );
+
+        console.log('Data inserted successfully:', result.rows[0]);
+        res.status(200).send('Data received and inserted successfully.');
+    } catch (error) {
+        console.error('Error inserting data into PostgreSQL:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
 // Endpoint to get the latest data
 app.get('/latest-data', async (req, res) => {
     try {
         const result = await pool.query(
             'SELECT * FROM sensor_data ORDER BY timestamp DESC LIMIT 1'
+        );
+
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error fetching latest data from PostgreSQL:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/latest-data-node2', async (req, res) => {
+    try {
+        const result = await pool.query(
+            'SELECT * FROM sensor_data_2 ORDER BY timestamp DESC LIMIT 1'
         );
 
         res.status(200).json(result.rows[0]);
@@ -55,6 +167,9 @@ app.post('/post-data', async (req, res) => {
             return res.status(400).json({ error: 'Invalid data format' });
         }
 
+        // Get the current epoch time
+        const epoch = Math.floor(Date.now() / 1000);
+
         // Insert data into PostgreSQL database
         const result = await pool.query(
             'INSERT INTO act_data (timestamp, red, blue, green) VALUES (NOW(), $1, $2, $3) RETURNING *',
@@ -72,7 +187,7 @@ app.post('/post-data', async (req, res) => {
 
         const otherServerData = {
             'm2m:cin': {
-                'con': `${red},${green},${blue},1`
+                'con': `${epoch},${red},${green},${blue},1`
             }
         };
 
@@ -88,6 +203,7 @@ app.post('/post-data', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
 
 
 // Start the server
